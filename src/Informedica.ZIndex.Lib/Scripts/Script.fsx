e6087a0c095f5d34c725064d3c5dc1a4b5ec3842
Casper Bollen <halcwb@Caspers-MacBook-Pro.local>
2024-02-18 13:25:35 +0100

    chore: sync commit

--- src/Informedica.ZIndex.Lib/Scripts/Script.fsx
+++ src/Informedica.ZIndex.Lib/Scripts/Script.fsx
@@ -376,7 +376,7 @@ printfn "Loading Substance"
 Substance.load ()
 
 
-GenPresProduct.filter "fenylefrine" "" "oculair"
+GenPresProduct.filter "magnesiumsulfaat" "" ""
 |> Array.collect (_.GenericProducts)
 |> Array.map (fun gp ->
     let subst = gp.Substances |> Array.distinctBy _.SubstanceId
