83b6949c106cdc0bd6e812f4eb32390f5c836688
Casper Bollen <halcwb@Caspers-MacBook-Pro.local>
2024-03-11 17:32:53 +0100

    chore: updated the open ai notebooks

--- src/Informedica.OpenAI.Lib/Notebooks/load.fsx
+++ src/Informedica.OpenAI.Lib/Notebooks/load.fsx
@@ -1,6 +1,11 @@
-# r "nuget: Newtonsoft.Json"
+
+#r "nuget: Newtonsoft.Json"
+#r "nuget: NJsonSchema"
 
 #r "../bin/Debug/net8.0/Informedica.Utils.Lib.dll"
 
+#load "../Utils.fs"
+#load "../OpenAI.fs"
+#load "../Texts.fs"
 #load "../Prompts.fs"
 #load "../Ollama.fs"
