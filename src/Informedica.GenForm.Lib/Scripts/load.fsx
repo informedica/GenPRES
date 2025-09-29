d554b91f804b74955eb481b5bfaad6d508b5aa58
halcwb <halcwb@gmail.com>
2025-09-29 19:05:06 +0200

    chore: fixing scripts

--- src/Informedica.GenForm.Lib/Scripts/load.fsx
+++ src/Informedica.GenForm.Lib/Scripts/load.fsx
@@ -8,8 +8,8 @@
 #r "nuget: ClosedXML"
 #r "nuget: FsToolkit.ErrorHandling"
 
-
 #r "../../Informedica.Utils.Lib/bin/Debug/net9.0/Informedica.Utils.Lib.dll"
+#r "../../Informedica.Logging.Lib/bin/Debug/net9.0/Informedica.Logging.Lib.dll"
 #r "../../Informedica.GenUnits.Lib/bin/Debug/net9.0/Informedica.GenUnits.Lib.dll"
 #r "../../Informedica.ZIndex.Lib/bin/Debug/net9.0/Informedica.ZIndex.Lib.dll"
 #r "../../Informedica.ZForm.Lib//bin/Debug/net9.0/Informedica.ZForm.Lib.dll"
